24c50aa4b748a3401073f805e2b5e0c0116a7cdd
Swaleh Hussein <hus.swaleh@yahoo.com>
2018-08-17 22:24:16 +0300

    Adding textviews using while loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -32,75 +32,18 @@ public class NumbersActivity extends AppCompatActivity {
 
         //Linking LinearLayout to java using id
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
-        //creating TextView
-        TextView wordView = new TextView(this);
-        //setting text in the wordView TextView
-        wordView.setText(englishNumbers.get(0));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView);
 
-        //creating TextView
-        TextView wordView2 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView2.setText(englishNumbers.get(1));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView2);
+        int index = 0;
 
-        //creating TextView
-        TextView wordView3 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView3.setText(englishNumbers.get(2));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView3);
+        while (index<englishNumbers.size()){
+            //creating new texview
+            TextView wordView = new TextView(this);
+            //setting text in the wordView TextView
+            wordView.setText(englishNumbers.get(index));
+            //displaying wordView TextView in the parent rootView Layout
+            rootView.addView(wordView);
+            index++;
+        }
 
-        //creating TextView
-        TextView wordView4 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView4.setText(englishNumbers.get(3));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView4);
-
-        //creating TextView
-        TextView wordView5 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView5.setText(englishNumbers.get(4));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView5);
-
-        //creating TextView
-        TextView wordView6 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView6.setText(englishNumbers.get(5));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView6);
-
-        //creating TextView
-        TextView wordView7 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView7.setText(englishNumbers.get(6));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView7);
-
-        //creating TextView
-        TextView wordView8 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView8.setText(englishNumbers.get(7));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView8);
-
-        //creating TextView
-        TextView wordView9 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView9.setText(englishNumbers.get(8));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView9);
-
-        //creating TextView
-        TextView wordView10 = new TextView(this);
-        //setting text in the wordView TextView
-        wordView10.setText(englishNumbers.get(9));
-        //displaying wordView TextView in the parent rootView Layout
-        rootView.addView(wordView10);
-        
     }
 }
