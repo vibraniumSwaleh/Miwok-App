6da913ecfdaaf0ae4508d02a5cd03df7a2530b6c
Swaleh Hussein <hus.swaleh@yahoo.com>
2018-08-19 01:22:32 +0300

    Added list names in all Activities

--- app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyMembersActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class FamilyMembersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_members);
+
+        //Array of family members
+        ArrayList<Word> familyMembersName = new ArrayList<Word>();
+
+        //Assigning words to englishTranslation array
+        familyMembersName.add(new Word("father", "әpә"));
+        familyMembersName.add(new Word("mother", "әṭa"));
+        familyMembersName.add(new Word("son", "angsi"));
+        familyMembersName.add(new Word("daughter", "tune"));
+        familyMembersName.add(new Word("older brother", "taachi"));
+        familyMembersName.add(new Word("younger brother", "chalitti"));
+        familyMembersName.add(new Word("older sister", "teṭe"));
+        familyMembersName.add(new Word("younger sister", "kolliti"));
+        familyMembersName.add(new Word("grandmother", "ama"));
+        familyMembersName.add(new Word("grandfather", "paapa"));
+
+
+        FamilyMemberAdapter itemsAdapter = new FamilyMemberAdapter(this, familyMembersName);
+
+        ListView listView = (ListView) findViewById(R.id.familymemberslist);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        //Array of phrases
+        ArrayList<Word> phrasesName = new ArrayList<Word>();
+
+        //Assigning words to englishTranslation array
+        phrasesName.add(new Word("Where are you going?", "minto wuksus"));
+        phrasesName.add(new Word("What is your name?", "tinnә oyaase'nә"));
+        phrasesName.add(new Word("My name is...", "oyaaset..."));
+        phrasesName.add(new Word("How are you feeling?", "michәksәs?"));
+        phrasesName.add(new Word("I’m feeling good.", "kuchi achit"));
+        phrasesName.add(new Word("Are you coming?", "әәnәs'aa?"));
+        phrasesName.add(new Word("Yes, I’m coming.", "hәә’ әәnәm"));
+        phrasesName.add(new Word("I’m coming.", "әәnәm"));
+        phrasesName.add(new Word("Let’s go.", "yoowutis"));
+        phrasesName.add(new Word("Come here.", "әnni'nem"));
+
+
+        PhrasesAdapter itemsAdapter = new PhrasesAdapter(this, phrasesName);
+
+        ListView listView = (ListView) findViewById(R.id.phraseslist);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
