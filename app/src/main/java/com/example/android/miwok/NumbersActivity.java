2ba7199987317dbee622d0884457881fe377adbe
Swaleh Hussein <hus.swaleh@yahoo.com>
2018-09-16 02:33:05 +0300

    Added Audio Focus in Number Activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,18 +1,19 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
     private MediaPlayer soundMedia;
+    private AudioManager audioManager;
     private MediaPlayer.OnCompletionListener mComleptionListener = new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
@@ -20,14 +21,31 @@ public class NumbersActivity extends AppCompatActivity {
         }
     };
 
+    private AudioManager.OnAudioFocusChangeListener audioManagerListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                soundMedia.start();
+                soundMedia.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                soundMedia.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                releaseMediaPlayer();
+            }
+
+        }
+    };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
+        //Creating Audio Manger object
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
         //Array of numbers
         final ArrayList<Word> words = new ArrayList<Word>();
-
         //Assigning words to englishTranslation array
         words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
         words.add(new Word("two", "ottiko", R.drawable.number_two, R.raw.number_two));
@@ -50,14 +68,17 @@ public class NumbersActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Word itemClicked = words.get(i);
                 releaseMediaPlayer();
-                soundMedia = MediaPlayer.create(NumbersActivity.this, itemClicked.getRawAudioSound());
-                soundMedia.start();
-                soundMedia.setOnCompletionListener(mComleptionListener);
+                Word itemClicked = words.get(i);
+                int result = audioManager.requestAudioFocus(audioManagerListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    soundMedia = MediaPlayer.create(NumbersActivity.this, itemClicked.getRawAudioSound());
+                    soundMedia.start();
+                    soundMedia.setOnCompletionListener(mComleptionListener);
+                }
             }
         });
-
     }
 
     @Override
@@ -80,6 +101,7 @@ public class NumbersActivity extends AppCompatActivity {
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
             soundMedia = null;
+            audioManager.abandonAudioFocus(audioManagerListener);
         }
     }
 }
