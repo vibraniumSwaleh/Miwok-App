d431c1ea84f69cee71fc25536da5816a0550eb8b
Swaleh Hussein <hus.swaleh@yahoo.com>
2018-09-17 15:17:08 +0300

    Added Tabs

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,15 +15,10 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,51 +29,17 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        //Linking view in xml to variable in java
-        TextView listenNumbers = (TextView)findViewById(R.id.numbers);
-        TextView listenFamilyMembers = (TextView)findViewById(R.id.family);
-        TextView listenColors = (TextView)findViewById(R.id.colors);
-        TextView lisenPhrases = (TextView)findViewById(R.id.phrases);
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(this, getSupportFragmentManager());
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
 
-        //Method to open Numbers Activity when view is click
-        listenNumbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick( View view){
-//              Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
-                Intent numbersIntents = new Intent(MainActivity.this, NumbersActivity.class);
-                startActivity(numbersIntents);
-            }
-        });
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
-        //Method to open Family Members Activity when view is click
-        listenFamilyMembers.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-//              Toast.makeText(view.getContext(), "Open the list of family members", Toast.LENGTH_SHORT).show();
-                Intent listenFamilyMembers = new Intent(MainActivity.this, FamilyMembersActivity.class);
-                startActivity(listenFamilyMembers);
-            }
-        });
-
-        //Method to open Colors Activity when view is click
-        listenColors.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-//              Toast.makeText(view.getContext(), "Open the list of colors", Toast.LENGTH_SHORT).show();
-                Intent listenColors = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(listenColors);
-            }
-        });
-
-        //Method to open Phrases Activity when view is click
-        lisenPhrases.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-//              Toast.makeText(view.getContext(), "Open the list of phrases", Toast.LENGTH_SHORT).show();
-                Intent lisenPhrases = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(lisenPhrases);
-            }
-        });
     }
 
 }
---
+++ app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    /** Context of the app */
+    private Context mContext;
+
+    public SimpleFragmentPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public android.support.v4.app.Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1) {
+            return new FamilyMembersFragment();
+        } else if (position == 2) {
+            return new PhrasesFragment();
+        } else if (position == 3) {
+            return new ColorsFragment();
+        } else return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
+}}
